d760ea2746af68a6f140943df0db7db419b17b66
Serdar Ağlamış <serdaraglamis@gmail.com>
2017-10-11 21:32:02 +0300

    Page layout component

---
+++ page/src/components/page-layout/page-layout.tsx
@@ -0,0 +1,28 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'page-layout',
+  styleUrl: 'page-layout.scss'
+})
+export class PageLayout {
+
+  @Prop() direction: string;
+  @Prop() width: string;
+  layoutStyle: {};
+
+  componentWillLoad() {
+    console.log('The component is about to be rendered', this.direction, this.width);
+    this.direction === 'vertical' ? this.layoutStyle = {
+      width: this.width,
+      maxWidth: this.width
+    } : null;
+  }
+
+  render() {
+    return (
+      <div class={this.direction} style={this.layoutStyle}>
+        <slot></slot>
+      </div>
+    );
+  }
+}
